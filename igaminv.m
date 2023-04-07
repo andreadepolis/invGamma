288c918abfbdccfc59c92ce2fa065ffd6c28265b
andreadepolis <andrea.depolis.17@mail.wbs.ac.uk>
2023-04-07 18:41:03 +0100

    Update igaminv.m
    Fixed some bugs

--- igaminv.m
+++ igaminv.m
@@ -13,4 +13,4 @@ function q = igaminv(n, alpha, betta)
 % 
 % Andrea De Polis, 2022 (a.de-polis@warwick.ac.uk)
  
-q = betta./gammaincinv(gamma(alpha)*(1-n), alpha) ;
+q = betta./gammaincinv(n, alpha, 'upper') ;
